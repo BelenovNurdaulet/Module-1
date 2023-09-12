fd1ab92b8a943c10936db773c13c0b0f3b2d4c5c
belen <belen@BELENOV>
2023-09-12 23:53:57 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Exmpl05() {
+            Console.WriteLine("ФИО пользователя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Добро пожаловать {0}" , name);
+            Console.ReadKey(true);
+        }
+        static void Exmpl06()
+        {
+            Console.WriteLine("Введите 3 числа: ");
+            string string01 = Console.ReadLine(); 
+            int int01 = Int32.Parse(string01);
+            string string02 = Console.ReadLine(); 
+            int int02 = Int32.Parse(string02);
+            string string03 = Console.ReadLine(); 
+            int int03 = Int32.Parse(string03);
+            
+            Console.WriteLine("Сумма чисел {0}" , int01 + int02 + int03);
+            Console.ReadKey(true);
+        }
+
+        static void Main(string[] args)
+    {
+        Exmpl06();
+    }
+
+}
+
+}
+
+
